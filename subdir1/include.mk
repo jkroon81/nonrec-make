dbd6ddaf1e4bc1cdc33036c0452d7c8f27168966
Jacob Kroon <jacob.kroon@gmail.com>
2016-08-13 21:15:24 +0200

    Another binary

--- subdir1/include.mk
+++ subdir1/include.mk
@@ -1,6 +1,9 @@
-bin = test2
+bin = test2 whoop
 test2-sources = main2.c kalle/puh.S
 test2-libs = $(builddir)/../subdir2/libfoo.a
 test2-ccflags = -I$(srcdir)/../subdir2
 test2-kalle/puh.S-asflags := -g
+whoop-sources = main2.c
+whoop-ccflags = -I$(srcdir)/../subdir2
+whoop-libs = $(builddir)/../subdir2/libfoo.a
 subdir = subdir2
