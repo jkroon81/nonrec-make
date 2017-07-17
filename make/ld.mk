88b00917596ba145563552047bf4e3115d447f6f
Jacob Kroon <jacob.kroon@gmail.com>
2017-07-17 23:39:20 +0200

    Cleanups

--- make/ld.mk
+++ make/ld.mk
@@ -17,15 +17,12 @@ b-dep := objdump
 
 .PHONY : objdump
 
-ld-staticlib-filename = $(call relpath,$(dir $1)lib$(notdir $1).a)
-ld-staticlib-shortname = $(patsubst lib%.a,%,$(notdir $1))
+ld-staticlib-filename  = $(call relpath,$(dir $1)lib$(notdir $1).a)
 ifeq ($(os),Windows_NT)
 ld-sharedlib-filename  = $(call relpath,$(dir $1)$(notdir $1).dll)
-ld-sharedlib-shortname = $(patsubst %.dll,%,$(notdir $1))
 else
 ifeq ($(os),GNU/Linux)
 ld-sharedlib-filename  = $(call relpath,$(dir $1)lib$(notdir $1).so)
-ld-sharedlib-shortname = $(patsubst lib%.so,%,$(notdir $1))
 else
 $(error Unsupported OS '$(os)')
 endif
@@ -37,29 +34,26 @@ $(if $(filter $(origin add-ld-$3-source),undefined),\
 cleanfiles += $2.[$(subst $(space),,\
   $(sort $($3-built-suffixes) $($3-extra-suffixes)))]
 mkdirs += $(call bpath,$2/..)
-$(addprefix $(builddir)/$2.,$($3-built-suffixes)) : \
-  $(makefile-deps) $(call if-arg,|,$(filter-out .,$(call bpath,$2/..)))
+$($3-built-suffixes:%=$(builddir)/$2.%) : $(makefile-deps) \
+  $(call if-arg,|,$(filter-out .,$(call bpath,$2/..)))
 $(foreach s,$($3-built-suffixes),$(eval $($s-dep) : $(builddir)/$2.$s))
 $(call add-ld-$3-source,$1,$2)
 endef
 
-add-ld-sources = $(if $2,$(call add-ld-sources-real,$1,$2,$3,$4))
+add-ld-sources = $(if $3,$(call $0-real,$1,$2,$3,$4,$5))
 define add-ld-sources-real
-$(eval $(call add-ld-$3-$4,$1))
-$(eval $(call add-ld-$3-sources,$1,$2,$4))
+$(eval $(call add-ld-$4-$5,$1,$2))
+$(eval $(call add-ld-$4-sources,$1,$2,$3,$5))
 endef
 
 define add-ld-header
-$(eval $1-staticlibs := $(call map,ld-staticlib-filename,$($1-staticlibs)))
-$(eval $1-sharedlibs := $(call map,ld-sharedlib-filename,$($1-sharedlibs)))
 mkdirs += $(call bpath,$1/..)
 $(foreach t,$(src-fmts),$(eval \
-  $(call add-ld-sources,$1,$(filter %.$t,$($1-sources)),$t,$2)))
+  $(call add-ld-sources,$1,$2,$(filter %.$t,$($1-sources)),$t,$3)))
 $(foreach s,$($1-sources),$(eval \
   $(call add-ld-source,$1,$(basename $s),$(patsubst .%,%,$(suffix $s)))))
-$(eval $(call tflags,$1,objs) += $(call map,relpath,$($1-staticlibs)))
-$(foreach l,$($1-sharedlibs),\
-  $(eval $(call add-ld-sharedlib-dep,$1,$(call relpath,$l),$2)))
+$(foreach t,static shared,$(foreach l,$($1-$tlibs),$(eval \
+  $(call add-ld-$tlib-dep,$1,$(call ld-$tlib-filename,$l),$(notdir $l),$3))))
 all : $(builddir)/$1
 $(builddir)/$1 : $($(call tflags,$1,objs)) $(makefile-deps) \
   $(call if-arg,|,$(filter-out .,$(call bpath,$1/..)))
@@ -71,17 +65,17 @@ add-ld-footer = $(foreach v,$(ld-target-vars),\
   $(eval undefine $1-$v) \
   $(eval undefine $(call tflags,$1,$v-append)))
 
+add-ld-staticlib-dep = $(call tflags,$1,objs) += $2
+
 define add-ld-sharedlib-dep
 $(builddir)/$1 : $2
-$(call tflags,$1,ldflags-append) += \
-  -L$(dir $2) \
-  -l$(call ld-sharedlib-shortname,$2)
-$(call $0-$3-$(os),$1,$2)
+$(call tflags,$1,ldflags-append) += -L$(dir $2) -l$3
+$(call $0-$4-$(os),$1,$2)
 endef
 
 define add-ld-sharedlib-dep-bin-Windows_NT
-$(builddir)/$1 : $(call relpath,$(builddir)/$(dir $1)$(notdir $2))
-$(call add-hardlink,$(call relpath,$(dir $1)$(notdir $2)),$2)
+$(builddir)/$1 : $(builddir)/$(dir $1)$(notdir $2)
+$(call add-hardlink,$(dir $1)$(notdir $2),$2)
 endef
 
 define add-ld-sharedlib-dep-bin-GNU/Linux
@@ -89,7 +83,7 @@ $(call tflags,$1,ldflags-append) += -Wl,-rpath=$(abspath $(dir $2))
 endef
 
 define add-ld-bin
-$(call add-ld-header,$1,bin)
+$(call add-ld-header,$1,$1,bin)
 $(call collect-flags,$1,ldflags,LDFLAGS)
 $(builddir)/$1 :
 	$$(if $$(_$$@-linker),,$$(error No linker for target '$$@'))
@@ -97,16 +91,15 @@ $(builddir)/$1 :
 $(call add-ld-footer,$1,bin)
 endef
 
-define add-ld-lib
-$(foreach f,$(ld-target-vars),\
-  $(eval $(call ld-$2-filename,$1)-$f := $($1-$f))\
-  $(eval undefine $1-$f))
-$(call add-ld-$2-real,$(call ld-$2-filename,$1))
+add-ld-lib = $(call $0-real,$(call ld-$2-filename,$1),$1,$2)
+define add-ld-lib-real
+$(foreach f,$(ld-target-vars),$(eval $1-$f := $($2-$f))$(eval undefine $2-$f))
+$(call add-ld-$3-real,$1,$2)
 endef
 
 add-ld-staticlib = $(call add-ld-lib,$1,staticlib)
 define add-ld-staticlib-real
-$(call add-ld-header,$1,staticlib)
+$(call add-ld-header,$1,$2,staticlib)
 $(builddir)/$1 :
 	$$(q)rm -f $$@
 	$$(AR_v) cDrs $$@ $$(_$$@-objs)
@@ -116,7 +109,7 @@ endef
 add-ld-sharedlib = $(call add-ld-lib,$1,sharedlib)
 define add-ld-sharedlib-real
 $(eval $(call tflags,$1,ccflags-append) += -fpic)
-$(call add-ld-header,$1,sharedlib)
+$(call add-ld-header,$1,$2,sharedlib)
 $(call collect-flags,$1,ldflags,LDFLAGS)
 $(builddir)/$1 :
 	$$(if $$(_$$@-linker),,$$(error No linker for target '$$@'))
--- make/vala-source.mk
+++ make/vala-source.mk
@@ -22,43 +22,39 @@ add-symlink-if-exists = $(if $(wildcard $2),$(call add-symlink,$1,$2))
 define add-ld-vala-source
 $(if $(vpath-build),$(call add-symlink-if-exists,$2.vala,$(srcdir)/$2.vala))
 $(builddir)/$2.c : $(builddir)/$1.vala-stamp
-	@true
 $(call add-ld-source,$1,$2,c)
 endef
 
-define add-vala-libs
+define add-vala-lib-deps
 $(eval $(call collect-flags,$1,vala-$2libs))
+$(eval $1-$2libs += $($(call tflags,$1,vala-$2libs)))
 $(foreach l,$($(call tflags,$1,vala-$2libs)),$(eval $(call add-vala-lib-dep
-  ,$1,$(call ld-$2lib-filename,$l),$(call ld-$2lib-shortname,$l),$2)))
+  ,$1,$(call ld-$2lib-filename,$l),$(notdir $l))))
 undefine $(call tflags,$1,vala-$2libs)
 endef
 
 define add-vala-lib-dep
 $1-valaflags += --pkg=$3 --vapidir=$(dir $2)
 $1-ccflags += -I$(dir $2)
-$1-$4libs += $2
 $(builddir)/$1.vala-stamp : $2.vala-stamp
 endef
 
 define add-ld-vala-sources
-$(builddir)/$1.vala-stamp : $(addprefix $(builddir)/,$2) $(makefile-deps)
-	$$(VALAC_v) --ccode $$(_$$(@:.vala-stamp=)-valaflags) \
-	  $(addprefix $(builddir)/,$2)
+$(builddir)/$1.vala-stamp : $(3:%=$(builddir)/%) $(makefile-deps)
+	$$(VALAC_v) --ccode $$($(call tflags,$1,valaflags)) $(3:%=$(builddir)/%)
 	$(q)touch $$@
 cleanfiles += $1.vala-stamp
-$(foreach t,static shared,$(eval $(call add-vala-libs,$1,$t)))
+$(foreach t,static shared,$(eval $(call add-vala-lib-deps,$1,$t)))
 $(eval $(call tflags,$1,ccflags-append) += $(glib-ccflags))
 $(call collect-flags,$1,valaflags,VALAFLAGS)
-$(call add-ld-sources,$1,$(patsubst %.vala,%.c,$2),c,$3)
+$(call add-ld-sources,$1,$2,$(patsubst %.vala,%.c,$3),c,$4)
 endef
 
 add-ld-vala-bin = $(call tflags,$1,ldflags-append) += $(glib-ldflags)
 
-add-ld-vala-staticlib = $(call add-ld-vala-lib \
-  ,$1,$(call ld-staticlib-shortname,$1),static)
+add-ld-vala-staticlib = $(call add-ld-vala-lib,$1,$2,static)
 
-add-ld-vala-sharedlib = $(call add-ld-vala-lib \
-  ,$1,$(call ld-sharedlib-shortname,$1),shared)
+add-ld-vala-sharedlib = $(call add-ld-vala-lib,$1,$2,shared)
 
 define add-ld-vala-lib
 $1-valaflags += --vapi=$(builddir)/$2.vapi \
@@ -76,7 +72,6 @@ endef
 
 define add-vala-gir
 $(builddir)/$2-$3.gir : $(builddir)/$1.vala-stamp
-	@true
 $1-valaflags += --gir=$(builddir)/$2-$3.gir
 cleanfiles += $(addprefix $2-$3.,gir typelib typelib.c)
 $(call add-ld-source,$1,$2-$3.typelib,c)
