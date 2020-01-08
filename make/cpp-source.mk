212a17fff294ba8232ed21244243f6bab44276a1
Jacob Kroon <jacob.kroon@gmail.com>
2020-01-08 21:28:30 +0100

    Rename 'cppflags' to 'cxxflags'

--- make/cpp-source.mk
+++ make/cpp-source.mk
@@ -8,14 +8,14 @@ $(eval $(call add-vcmd,CXXPP,,$$(CXX)))
 $(if $(vpath-build),$(eval vpath %.cpp $(top-srcdir)))
 
 %.o : %.cpp
-	$(CXX_v) -c -MMD -MP $(_$@-cppflags) $< -o $@
+	$(CXX_v) -c -MMD -MP $(_$@-cxxflags) $< -o $@
 %.s : %.cpp
-	$(CXXAS_v) -S $(_$*.o-cppflags) $< -o $@
+	$(CXXAS_v) -S $(_$*.o-cxxflags) $< -o $@
 %.i : %.cpp
-	$(CXXPP_v) -E -P $(_$*.o-cppflags) $< -o $@
+	$(CXXPP_v) -E -P $(_$*.o-cxxflags) $< -o $@
 
-subdir-vars        += cppflags
-ld-target-vars     += cppflags
+subdir-vars        += cxxflags
+ld-target-vars     += cxxflags
 cpp-built-suffixes := b i o s
 cpp-extra-suffixes := d
 
@@ -28,9 +28,9 @@ add-ld-cpp-sources = $(call tflags,$1,linker) = $$(CXXLD_v)
 
 define add-ld-cpp-source
 $(if $(skip-deps),,-include $(builddir)/$2.d)
-$(call collect-flags,$2.o,cppflags,CXXFLAGS,$1)
+$(call collect-flags,$2.o,cxxflags,CXXFLAGS,$1)
 $(call tflags,$1,objs) += $(call bpath,$2.o)
 $(call bpath,$2.i) $(call bpath,$2.s) : $(call bpath,$2.o)
 endef
 
-add-ld-cpp-sharedlib = $(call tflags,$1,cppflags-append) += -fpic
+add-ld-cpp-sharedlib = $(call tflags,$1,cxxflags-append) += -fpic
